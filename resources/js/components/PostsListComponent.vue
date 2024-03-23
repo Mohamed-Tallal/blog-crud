4742aa9a577269f3cbf0ac7e47a02ed7238a7cb6
Mohamed-Tallal <mohamedtallal28@gmail.com>
2024-03-23 07:48:24 +0200

    search in blog

--- resources/js/components/PostsListComponent.vue
+++ resources/js/components/PostsListComponent.vue
@@ -8,10 +8,27 @@
               <router-link class="group flex items-center rounded-md text-blue-500 border border-blue-500 hover:border-blue-400 hover:bg-blue-50 text-sm font-medium px-3 py-2 shadow-sm" :to="{ name: 'user.post.index' }"> Dashboard</router-link>
             </div>
             <form class="group relative">
-              <svg width="20" height="20" fill="currentColor" class="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                />
               </svg>
-              <input class="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="Filter projects...">
+              <input
+                class="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm"
+                type="text"
+                aria-label="Filter posts"
+                placeholder="Filter posts..."
+                v-model="search"
+                @input="handleSearchInput"
+              >
             </form>
         </header>
         <div v-if="posts.length > 0">
@@ -42,19 +59,29 @@
     </div>
 </template>
 
+
 <script setup>
-  import useBlog from '@/composables/blog'
-  import { onMounted } from 'vue'
+import useBlog from '@/composables/blog'
+import { onMounted, watchEffect } from 'vue'
 
-  const { posts, listPosts } = useBlog()
-  
-  onMounted(listPosts)
+const { posts, listPosts } = useBlog()
+let search = '';
 
-  function truncateString(str, num) {
-    if (str.length <= num) {
-      return str;
-    }
-    return str.slice(0, num) + '.....';
+onMounted(() => {
+  listPosts(search)
+})
+
+function truncateString(str, num) {
+  if (str.length <= num) {
+    return str;
   }
+  return str.slice(0, num) + '.....';
+}
 
+// Handler for search input
+function handleSearchInput(event) {
+  search = event.target.value;
+  listPosts(search)
+
+}
 </script>
--- resources/js/composables/blog.js
+++ resources/js/composables/blog.js
@@ -6,18 +6,28 @@ export default function useBlog() {
     const post = ref([])
     const posts = ref([])
 
-    const listPosts = async () => {
-        let response = await axios.get('/api/blog')
-        posts.value = response.data.data
-        console.log(response.data.data);
-    }
- 
+
+    const listPosts = async (searchTerm) => {
+        try {
+          const response = await axios.get('/api/blog', {
+            params: {
+              search: searchTerm
+            }
+          })
+          posts.value = response.data.data
+          console.log('getPosts')
+        } catch (error) {
+          console.error(error)
+        }
+      }
+
     const showPost = async (id) => {
         let response = await axios.get(`/api/blog/${id}`)
         post.value = response.data.data
         console.log(response.data.data);
 
     }
+
  
     return {
         post,
