3afb12f8a3dcf6031b3a5732839e990e880e2137
Mohamed-Tallal <mohamedtallal28@gmail.com>
2024-03-25 21:29:13 +0200

    add arabic translate file

--- resources/js/components/dashboard/PostsComponent.vue
+++ resources/js/components/dashboard/PostsComponent.vue
@@ -45,10 +45,10 @@
             <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
                 <router-link :to="{ name: 'user.post.edit', params: { id: item.id } }"
                     class="mr-2 inline-flex items-center group flex items-center rounded-md text-green-500 border border-green-500 hover:border-green-400 hover:bg-green-50 text-sm font-medium px-3 py-2 shadow-sm">
-                    Edit
+                    {{ $t('edit') }}
                 </router-link>
                 <button @click="deleteMyPost(item.id)" class="inline-flex items-center group flex items-center rounded-md text-red-500 border border-red-500 hover:border-red-400 hover:bg-red-50 text-sm font-medium px-3 py-2 shadow-sm">
-                    Delete
+                    {{ $t('delete') }}
                 </button>
             </td>
         </tr>
